339c67bed4a313ab7584b6e141fb5bd1d2499980
theOneSun <sunjian_better@163.com>
2018-01-12 14:50:27 +0800

    将路由配置从main.js拆分到router/index.js中

--- src/main.js
+++ src/main.js
@@ -2,54 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App.vue'
-import Hi from './components/Hi.vue'
 import VueRouter from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import One from './components/One.vue'
-import Two from './components/Two.vue'
-import Home from './components/Home.vue'
 import ElementUI from 'element-ui'
-import Show from './components/second/Show.vue'
-import HomeIndex from './components/second/HomeIndex.vue'
+import Routes from './router/index'
 
 Vue.use(VueRouter)
 Vue.use(ElementUI)
 
-const routes = [
-  {
-    path: '/',
-    component: Home,
-    children: [
-      {
-        path: '/',
-        component: HomeIndex
-      }
-    ]
-  },
-  {
-    path: '/home',
-    component: Home,
-    children: [
-      {
-        path: '/',
-        component: HomeIndex
-      },
-      {
-        path: 'show',
-        component: Show
-      }
-    ]
-  },
-  {path: '/hi', component: Hi},
-  {path: '/one', component: One},
-  {path: '/two', component: Two},
-  {path: '/hello', component: HelloWorld}
-]
-
 // 3. 创建 router 实例，然后传 `routes` 配置
 // 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
-  routes // （缩写）相当于 routes: routes
+  // routes // （缩写）相当于 routes: routes
+  routes: Routes
 })
 
 // 4. 创建和挂载根实例。
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,40 @@
-// // index.js中的内容可以写到main.js中
-// import Vue from 'vue'
-// import Router from 'vue-router'
-// import HelloWorld from '../components/HelloWorld.vue'
-// import Hi from '../components/Hi.vue'
-// import App from '../App.vue'
-// import One from '../components/One.vue'
-// import Two from '../components/Two.vue'
-// import Home from '../components/Home.vue'
-//
-// Vue.use(Router)
-//
-// const routes = [
-//   {path: '/', component: Home},
-//   {path: '/hi', component: Hi},
-//   {path: '/one', component: One},
-//   {path: '/two', component: Two},
-//   {path: '/hello', component: HelloWorld}
-// ]
-//
-// // 3. 创建 router 实例，然后传 `routes` 配置
-// // 你还可以传别的配置参数, 不过先这么简单着吧。
-// const router = new Router({
-//   routes // （缩写）相当于 routes: routes
-// })
-//
-// new Vue({
-//   router,
-//   render: h => h(App)
-// }).$mount('#app')
+// index.js中的内容可以写到main.js中
+import HelloWorld from '../components/HelloWorld.vue'
+import Hi from '../components/Hi.vue'
+import One from '../components/One.vue'
+import Two from '../components/Two.vue'
+import Home from '../components/Home.vue'
+import HomeIndex from '../components/second/HomeIndex.vue'
+import Show from '../components/second/Show.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+    children: [
+      {
+        path: '/',
+        component: HomeIndex
+      }
+    ]
+  },
+  {
+    path: '/home',
+    component: Home,
+    children: [
+      {
+        path: '/',
+        component: HomeIndex
+      },
+      {
+        path: 'show',
+        component: Show
+      }
+    ]
+  },
+  {path: '/hi', component: Hi},
+  {path: '/one', component: One},
+  {path: '/two', component: Two},
+  {path: '/hello', component: HelloWorld}
+]
+export default routes
