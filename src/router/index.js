8ecaaca7668f5107022364c6d3131af8b35bc08d
theOneSun <sunjian_better@163.com>
2018-01-12 15:58:30 +0800

    删除没用的模块

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,4 @@
 // index.js中的内容可以写到main.js中
-import HelloWorld from '../components/HelloWorld.vue'
-import Hi from '../components/Hi.vue'
-import One from '../components/One.vue'
-import Two from '../components/Two.vue'
 import Home from '../components/Home.vue'
 import HomeIndex from '../components/second/HomeIndex.vue'
 import Show from '../components/second/Show.vue'
@@ -31,10 +27,6 @@ const routes = [
         component: Show
       }
     ]
-  },
-  {path: '/hi', component: Hi},
-  {path: '/one', component: One},
-  {path: '/two', component: Two},
-  {path: '/hello', component: HelloWorld}
+  }
 ]
 export default routes
